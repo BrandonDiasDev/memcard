aa2cb9a4e8efabac7a44f4ddde738dec83c535a0
Brandon Dias <brandon.dias@castgroup.com.br>
2025-01-11 23:56:48 -0300

    Firts creations in BD via Postman of users: sucess.

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,17 +1,11 @@
 const express = require("express");
-const User = require("../models/User.js");
-
 const router = express.Router();
 
+const User = require("../models/User.js");
+const createUser = require("../controllers/userController");
+
 // Criar usuário
-router.post("/", async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
+router.post('/', createUser);
 
 // Listar usuários
 router.get("/", async (req, res) => {
